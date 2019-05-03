431736ef5f7f26f959172cdc644277a392d36cae
Paul DeCarlo <toolboc@gmail.com>
2019-05-03 16:58:28 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# RetroArch-AI-with-IoTEdge
+Using IoTEdge with Cognitive Services Containers to enhance Retro Video Games 🎮
+
+This project attempts leverages AI services running side-by-side [Retroarch](https://www.retroarch.com/) on top of [Lakka](www.lakka.tv/
+) through the use of containers to allow for interesting interactions with Retroarch in a modular and remotely configurable fashion.
+
+## Current Modules
+
+**ScreenshotTranslator**
+
+[![Docker Pulls](https://img.shields.io/docker/pulls/toolboc/retroarch-screenshot-translator.svg?style=flat-square)](https://hub.docker.com/r/toolboc/retroarch-screenshot-translator)
+
+<img src="http://i.imgur.com/uYzisSO.jpg" width="600" />
+
+![Demo](https://i.imgur.com/sXnbjOi.gif)
+
+*Translates screenshots captured in /storage/screenshots/ to the framebuffer of fb0 using a local container instance of [cognitive-services-recognize-text](https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/computer-vision-how-to-install-containers#docker-pull-for-the-recognize-text-container)*
+
+| Parameter      | Description |   Type        |
+| -------------- | ------------| --------- |
+| Fontsize      | Controls the size of the rendered translation font (default: 25) | Integer  |
+| FontFamily   | Controls the FontFamily used to render the translation (default: DejaVuSansMono-Bold)   | string |
+| Language   | Controls the Language to translate text to (currently only translates from English)   | string |
+
+Ex: To translate to Japanese and display japanese characters, set the desired properties for the module twin of `ScreenshotTranslator` on the device in question for `FontFamily` to "TakaoPMincho" and `Language` to "ja".
+  
+<img src="http://i.imgur.com/lIo70jm.png" width="600" />
+<img src="http://i.imgur.com/u3haWXz.jpg" width="600" />
+
+FontFamily values are obtained from installed fonts in `/usr/share/fonts/truetype/*`
+
+Valid values include:
+* DejaVuSans-Bold 
+* DejaVuSans  
+* DejaVuSansMono-Bold  
+* DejaVuSansMono  
+* DejaVuSerif-Bold
+* DejaVuSerif
+* TakaoMincho (Japanese)
+* TakaoPMincho (Japanese) 
+* NanumGothicBold (Korean)
+* NanumMyeongjoBold (Korean)
+
+Language values can be obtained from the [Microsoft Text Translator Language Support Documentation](https://docs.microsoft.com/en-us/azure/cognitive-services/translator/language-support?wt.mc_id=RetroArchAIwithIoTEdge-github-pdecarlo)
+
+
+# Installation and Setup
+
+Requires an x64 compatible device with a screen and HDMI out and access to the [Cogntive Services Computer Vision Containers Preview](https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/computer-vision-how-to-install-containers#request-access-to-the-private-container-registry?wt.mc_id=RetroArchAIwithIoTEdge-github-pdecarlo).
+
+1 - Download the [latest Lakka.img.gz release from this fork of Lakka-LibreELEC](https://github.com/toolboc/Lakka-LibreELEC/releases/download/RetroArch-AI-with-IoTEdge_v1/Lakka-Generic.x86_64-2.2-RetroArch-AI-with-IoTEdge.img.gz) and install it by following these [instructions](http://www.lakka.tv/get/windows/generic/install/)).
+
+2 - After installation, configure the device to output the RetroArch UI over HDMI by select the appropriate monitor index in [Video Settings](http://www.lakka.tv/doc/Video-settings/), you may also want to configure audio output to route over HDMI as well by configuring the appropriate Audio Device in [Audio Settings](http://www.lakka.tv/doc/Audio-settings/).
+
+3 - Configure the Wifi settings to connect to an approprite access point in the [WiFi-Configuration Interface](http://www.lakka.tv/articles/2016/10/06/major-release-brings-wifi-and-simplified-interface/#wi-fi-configuration-interface)
+
+4 - SSH into your Lakka Device by following these [instructions](http://www.lakka.tv/doc/Accessing-Lakka-command-line-interface/) and execute the following commands to install the Docker Add-On:
+```
+cd ~/.kodi/
+
+wget https://github.com/toolboc/Lakka-LibreELEC/releases/download/RetroArch-AI-with-IoTEdge_v1/service.system.docker-8.2.122.zip
+
+unzip service.system.docker-8.2.122.zip
+
+cd ~/.kodi/addons/service.system.docker/system.d/
+
+systemctl enable service.system.docker.service
+
+reboot
+```
+Note: Docker will install to the storage partition and is accessible via the path:
+```
+/storage/.kodi/addons/service.system.docker/bin/docker
+```
+
+Verify that docker is running with:
+```
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/storage/.kodi/addons/service.system.docker/bin
+docker ps
+```
+
+5 - Create an Azure IoT Hub by following these [instructions](https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-create-through-portal?wt.mc_id=RetroArchAIwithIoTEdge-github-pdecarlo
+) then [create a new IoT Edge Device in the Azure Portal](https://docs.microsoft.com/en-us/azure/iot-edge/how-to-register-device-portal?wt.mc_id=RetroArchAIwithIoTEdge-github-pdecarlo) and retrieve the connection string.
+
+6 - SSH into your Lakka Device by following these [instructions](http://www.lakka.tv/doc/Accessing-Lakka-command-line-interface/) and verify that docker is running with:
+```
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/storage/.kodi/addons/service.system.docker/bin
+docker ps
+```
+Create an instance of an [azure-iot-edge-device-container](https://github.com/toolboc/azure-iot-edge-device-container) with (be sure to replace <IoTHubDeviceConnectionString> with the key obtained in Step 5):
+
+```
+docker run --name edge-device-container --restart always -d --privileged -v /storage/screenshots:/storage/screenshots -v /dev/fb0:/dev/fb0 -e connectionString='<IoTHubDeviceConnectionString>' toolboc/azure-iot-edge-device-container
+```
+
+7 - Install [Visual Studio Code](https://code.visualstudio.com/) onto an available development machine and install the [Azure IoT Edge extension for Visual Studio Code](https://marketplace.visualstudio.com/items?itemName=vsciot-vscode.azure-iot-edge?wt.mc_id=RetroArchAIwithIoTEdge-github-pdecarlo).  
+
+8 - Clone or download a copy of [this repo](https://github.com/toolboc/RetroArch-AI-with-IoTEdge) and open the `RetroArch-AI-with-IoTEdge` folder in Visual Studio Code.  Next, press `F1` and select `Azure IoT Hub: Select IoT Hub` and choose the IoT Hub you created in Step 5, follow the prompts to complete the process.
+
+9 - In VS Code, navigate to the `deployment.template.json` file and modify the following values:
+
+* `{YourPrivateRepoUsername}` - The username provided for access to the [Cogntive Services Computer Vision Containers Preview](https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/computer-vision-how-to-install-containers#request-access-to-the-private-container-registry?wt.mc_id=RetroArchAIwithIoTEdge-github-pdecarlo).
+* `{YourPrivateRepoPassword}` - The password provided for access to the [Cogntive Services Computer Vision Containers Preview](https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/computer-vision-how-to-install-containers#request-access-to-the-private-container-registry?wt.mc_id=RetroArchAIwithIoTEdge-github-pdecarlo).
+* `{YourCognitiveServicesApiKey}` - Follow these instruction to [create a Cognitive Services Resource in Azure](https://docs.microsoft.com/en-us/azure/cognitive-services/cognitive-services-apis-create-account?wt.mc_id=RetroArchAIwithIoTEdge-github-pdecarlo), obtain and replace with the respective key.
+* `{YourBillingEndpointLocale}` - The locale prefix used for the endpoint of your Cognitive Services Resource
+* `{YourTranslatorTextApiKey}` - Follow these instruction to [create a Cognitive Services Translator Text API Resource in Azure](https://docs.microsoft.com/en-us/azure/cognitive-services/translator/translator-text-how-to-signup?wt.mc_id=RetroArchAIwithIoTEdge-github-pdecarlo), obtain and replace with the respective key.
+
+10 - Create a deployment for the IoT Edge device by right-clicking `deployment.template.json` and select `Generate IoT Edge Deployment Manifest`.  This will create a file under the config folder named `deployment.amd64.json`, right-click that file and select `Create Deployment for Single Device` and select the device you created in Step 5.
+
+Wait a few minutes for the deployment to complete and you should see the logo for the RetroArch Screenshot Translator appear on the main screen of your Lakka machine.
+
+Configure a Hotkey or manually select "Take Screenshot" while playing a game, the translated image is stored to `/storage/screenshots/translated/` and displayed on the main screen of the Lakka Device.
---
+++ modules/ScreenshotTranslator/Program.cs
@@ -0,0 +1,371 @@
+namespace ScreenshotTranslator
+{
+    using System;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Runtime.Loader;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+    using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+    using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using System.Diagnostics;
+    using System.Drawing;
+    using Microsoft.Azure.Devices.Shared; 
+
+    class Program
+    {
+        static int counter;
+        static FileSystemWatcher watcher;
+        private const String logoImagePath = "/app/logo.png";
+        private const String screenShotPath ="/storage/screenshots/";
+        private const String translatedScreenShotPath="/storage/screenshots/translated/";
+        static string FontFamily = "DejaVuSansMono-Bold";
+        static int FontSize { get; set; } = 25;
+        static string Language { get; set; } = "en";
+        private static string TranslatorTextApiKey = null;
+        private const string TranslatorTextEndpoint = "https://api.cognitive.microsofttranslator.com";
+        private const string RecognizeTextEndpoint = "http://cognitive-services-recognize-text:5000";
+        private static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                TranslatorTextApiKey=args[0].Split("=")[1];
+            }
+            catch
+            {
+                Console.WriteLine("ERROR - Expected Argument for TranslatorTextApiKey");
+            }
+
+            if(TranslatorTextApiKey.Length != 32)
+                Console.WriteLine("WARNING - TranslatorTextApiKey is not set or formatted appropriately");
+
+            Init().Wait();
+
+            // Wait until the app unloads or is cancelled
+            var cts = new CancellationTokenSource();
+            AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
+            Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
+            WhenCancelled(cts.Token).Wait();
+        }
+
+        /// <summary>
+        /// Handles cleanup operations when app is cancelled or unloads
+        /// </summary>
+        public static Task WhenCancelled(CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// Initializes the ModuleClient and sets up the callback to receive
+        /// messages containing temperature information
+        /// </summary>
+        static async Task Init()
+        {
+            MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
+            ITransportSettings[] settings = { mqttSetting };
+
+            // Open a connection to the Edge runtime
+            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
+            await ioTHubModuleClient.OpenAsync();
+            Console.WriteLine("IoT Hub module client initialized.");
+
+            // Read the TemperatureThreshold value from the module twin's desired properties
+            var moduleTwin = await ioTHubModuleClient.GetTwinAsync();
+            await OnDesiredPropertiesUpdate(moduleTwin.Properties.Desired, ioTHubModuleClient);
+
+            // Attach a callback for updates to the module twin's desired properties.
+            await ioTHubModuleClient.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertiesUpdate, null);
+
+            // Register callback to be called when a message is received by the module
+            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, ioTHubModuleClient);
+
+            InitFileWatcher();
+
+            DisplayImage(logoImagePath);
+
+            Console.WriteLine("Successfully initialized ScreenshotWatcher module.");     
+        }
+
+        static Task OnDesiredPropertiesUpdate(TwinCollection desiredProperties, object userContext)
+        {
+            try
+            {
+                Console.WriteLine("Desired property change:");
+                Console.WriteLine(JsonConvert.SerializeObject(desiredProperties));
+
+                if (desiredProperties["FontSize"]!=null)
+                    FontSize = desiredProperties["FontSize"];
+
+                if (desiredProperties["Language"]!=null)
+                    Language = desiredProperties["Language"];
+
+                if (desiredProperties["FontFamily"]!=null)
+                    FontFamily = desiredProperties["FontFamily"];
+
+            }
+            catch (AggregateException ex)
+            {
+                foreach (Exception exception in ex.InnerExceptions)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Error when receiving desired property: {0}", exception);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Error when receiving desired property: {0}", ex.Message);
+            }
+            return Task.CompletedTask;
+        }        
+
+        private static void InitFileWatcher(){
+            
+            // Create a new FileSystemWatcher and set its properties.
+            watcher = new FileSystemWatcher();
+            watcher.Path = screenShotPath;
+
+            // Watch for changes in CreationTimes
+            watcher.NotifyFilter = NotifyFilters.FileName;
+
+            // Only watch text files.
+            watcher.Filter = "*.png";
+
+            // Add event handlers.
+            watcher.Created += OnChanged;
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+        }
+
+        // Define event handlers.
+        private static async void OnChanged(object source, FileSystemEventArgs e)
+        {
+        // Specify what is done when a file is changed, created, or deleted.
+        Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+
+            var image = e.FullPath;
+
+            if (!File.Exists(image))
+            {
+                Console.WriteLine($"Unable to open or read '{image}'");
+                Environment.Exit(-1);
+            }
+
+            try
+            {
+                //Wait for file to finish writing
+                await Task.Delay(1000);
+
+                Console.WriteLine($"Extracting text from '{image}'");
+                Stream imageStream = File.OpenRead(image);
+                var resultAsJson = await ExtractText(imageStream);
+                
+                //Wait for file to finish closing
+                await Task.Delay(1000);
+
+                imageStream = File.OpenRead(image);
+                
+                var translatedImage = await TranslateImage(new Bitmap(imageStream), resultAsJson, image.Remove(0,screenShotPath.Length));
+                DisplayImage(translatedImage);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Exception encountered processing: '{image}'");
+                Console.WriteLine(ex.Message);
+            }
+        
+        }
+        private static async Task<JObject> ExtractText(Stream imageStream)
+        {
+
+            String responseString = string.Empty;
+            using (var imageContent = new StreamContent(imageStream))
+            {
+                imageContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                var requestAddress = RecognizeTextEndpoint + "/vision/v2.0/recognizetextDirect";
+
+                using (var response = await client.PostAsync(requestAddress, imageContent))
+                {
+                    var resultAsString = await response.Content.ReadAsStringAsync();
+                    var resultAsJson = JsonConvert.DeserializeObject<JObject>(resultAsString);
+
+                    if (resultAsJson["lines"] == null)
+                    {
+                        responseString = resultAsString;
+                    }
+                    else
+                    {
+                        foreach (var line in resultAsJson["lines"])
+                        {
+                            responseString += line["text"] + "\n";
+                        }
+                    }
+
+                    Console.WriteLine(responseString);
+                    return resultAsJson;
+                }
+            }
+        }
+
+        private static async Task<String> TranslateImage(Image image, JObject resultAsJson, string fileName)
+        {
+            string translatedImage = translatedScreenShotPath + fileName;
+
+            Graphics graph = Graphics.FromImage(image);
+            
+            //Blackout original Text
+            foreach (var line in resultAsJson["lines"])
+            {          
+
+                // Create points that define polygon.
+                PointF point1 = new PointF((int)line["boundingBox"][0],(int)line["boundingBox"][1]);
+                PointF point2 = new PointF((int)line["boundingBox"][2],(int)line["boundingBox"][3]);
+                PointF point3 = new PointF((int)line["boundingBox"][4],(int)line["boundingBox"][5]);
+                PointF point4 = new PointF((int)line["boundingBox"][6],(int)line["boundingBox"][7]);
+                PointF[] curvePoints =
+                        {
+                            point1,
+                            point2,
+                            point3,
+                            point4,
+                        };
+                        
+                // Draw polygon curve to screen.
+                graph.FillPolygon(Brushes.Black, curvePoints);
+            }
+
+            //Translate and print Text
+            foreach (var line in resultAsJson["lines"])
+            {          
+                string translatedText = string.Empty;
+
+                if(Language != "en")
+                    translatedText = await TranslateText(line["text"] + "\n");
+                else
+                    translatedText = line["text"] + "\n";
+
+                int x = (int)line["boundingBox"][0];
+                int y = (int)line["boundingBox"][1];
+
+                graph.DrawString(translatedText, 
+                    new Font(new FontFamily(FontFamily), FontSize), 
+                    Brushes.White, new PointF(x, y));
+
+            }
+
+            System.IO.Directory.CreateDirectory(translatedScreenShotPath);
+            image.Save(translatedImage, System.Drawing.Imaging.ImageFormat.Png);
+            return translatedImage;
+        }
+
+        public static async Task<String> TranslateText(string text)
+        {
+            var requestAddress = TranslatorTextEndpoint + "/translate?api-version=3.0&to=" + Language;
+            System.Object[] body = new System.Object[] { new { Text = text }};
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var request = new HttpRequestMessage())
+            {
+                // Set the method to POST
+                request.Method = HttpMethod.Post;
+
+                // Construct the full URI
+                request.RequestUri = new Uri(requestAddress);
+
+                // Add the serialized JSON object to your request
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+                // Add the authorization header
+                request.Headers.Add("Ocp-Apim-Subscription-Key", TranslatorTextApiKey);
+
+                // Send request, get response
+                var response = client.SendAsync(request).Result;
+                var resultAsString = await response.Content.ReadAsStringAsync();
+                var resultAsJson = JsonConvert.DeserializeObject<JArray>(resultAsString);
+
+                // Print the response
+                Console.WriteLine("Text translated to " + Language);
+                Console.WriteLine(resultAsString);
+                string translatedText = string.Empty;
+                try
+                {
+                    translatedText = resultAsJson[0]["translations"][0]["text"] + "\n";
+                }
+                catch
+                {
+                    translatedText = "N/A";
+                }
+
+                return translatedText;
+            }
+        }
+
+        /// <summary>
+        /// Displays image to fb0 using fbi
+        /// </summary>
+        private static void DisplayImage(string image)
+        {        
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "fbi",
+                    Arguments = $"-d /dev/fb0 -T 1 -noverbose -a \"{image}\"",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            Console.WriteLine($"Displaying image: '{image}'");
+            process.Start();
+            process.WaitForExit();
+        }
+
+        /// <summary>
+        /// This method is called whenever the module is sent a message from the EdgeHub. 
+        /// It just pipe the messages without any change.
+        /// It prints all the incoming messages.
+        /// </summary>
+        static async Task<MessageResponse> PipeMessage(Message message, object userContext)
+        {
+            int counterValue = Interlocked.Increment(ref counter);
+
+            var moduleClient = userContext as ModuleClient;
+            if (moduleClient == null)
+            {
+                throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
+            }
+
+            byte[] messageBytes = message.GetBytes();
+            string messageString = Encoding.UTF8.GetString(messageBytes);
+            Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
+
+            if (!string.IsNullOrEmpty(messageString))
+            {
+                var pipeMessage = new Message(messageBytes);
+                foreach (var prop in message.Properties)
+                {
+                    pipeMessage.Properties.Add(prop.Key, prop.Value);
+                }
+                await moduleClient.SendEventAsync("output1", pipeMessage);
+                Console.WriteLine("Received message sent");
+            }
+            return MessageResponse.Completed;
+        }
+    }
+}
